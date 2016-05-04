b8c4cf8623256962120998da227e4577c467019c
Kristof Dewilde <Krivi@rMBP15.local>
2016-05-04 02:55:43 +0200

    Now consumes Google's Geocode API using Spring's RestTemplate
    ... as required by the project's specifications. Before this commit, Google Java API rather than JSON API was used which arguably was slightly more efficient.
    
    Also finished basic implementation of own REST API: can return all networks in JSON format.
    
    And do you know what it will return next?
    
    That's right.
    
    it's dat boi!!!
    
    o shit waddup!

---
+++ web/src/main/java/be/krivi/ucll/ip/web/rest/converter/Converter.java
@@ -0,0 +1,133 @@
+package be.krivi.ucll.ip.web.rest.converter;
+
+import be.krivi.ucll.ip.web.rest.model.Geocode;
+import be.krivi.ucll.ip.web.rest.model.Geometry;
+import be.krivi.ucll.ip.web.rest.model.Results;
+import be.krivi.ucll.ip.web.validation.NetworkForm;
+import com.google.maps.GeoApiContext;
+import com.google.maps.GeocodingApi;
+import com.google.maps.model.GeocodingResult;
+import com.sun.tools.javac.util.Pair;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by Krivi on 02/04/16.
+ */
+
+// Simple converter class that gets latitude and longitude for a given address.
+public class Converter{
+
+    private static final String API_KEY = "AIzaSyCE41BGnnoE6BvD8NNAKJ-OhrOhyb2rSnw";
+
+    //****************************************************************
+    // region Using Google Geocode Java API
+    //****************************************************************
+
+    public static Pair<Double, Double> getLatLon( NetworkForm networkForm ){
+        GeoApiContext context = new GeoApiContext().setApiKey( API_KEY );
+        GeocodingResult[] results;
+
+        try{
+            results = GeocodingApi.geocode(
+                    context,
+                    networkForm.getLocationAddress() + ", " +
+                            networkForm.getLocationZip() +
+                            networkForm.getLocationCity() + ", " +
+                            networkForm.getLocationCountry()
+            ).await();
+
+            return new Pair<>( results[0].geometry.location.lat, results[0].geometry.location.lng );
+        }catch( Exception e ){
+            return new Pair<>( 0.0, 0.0 );
+        }
+    }
+
+    public static Pair<Double, Double> getLatLon( String address ){
+        GeoApiContext context = new GeoApiContext().setApiKey( API_KEY );
+        GeocodingResult[] results;
+
+        try{
+            results = GeocodingApi.geocode(
+                    context,
+                    address
+            ).await();
+
+            return new Pair<>( results[0].geometry.location.lat, results[0].geometry.location.lng );
+        }catch( Exception e ){
+            return new Pair<>( 0.0, 0.0 );
+        }
+    }
+
+    //****************************************************************
+    // endregion
+    //****************************************************************
+
+    //****************************************************************
+    // region Using Google Geocode JSON API
+    //****************************************************************
+
+    public static Pair<Double, Double> getLatLonJson( NetworkForm networkForm ){
+        RestTemplate restTemplate = new RestTemplate();
+
+        try{
+            // https://maps.googleapis.com/maps/api/geocode/json?address=Herestraat+49,+3000+Leuven,+Belgium&key=AIzaSyCE41BGnnoE6BvD8NNAKJ-OhrOhyb2rSnw
+            Geocode geocode = restTemplate.getForObject(
+                    "https://maps.googleapis.com/maps/api/geocode/json?address=" +
+                            networkForm.getLocationAddress() + ", " +
+                            networkForm.getLocationZip() +
+                            networkForm.getLocationCity() + ", " +
+                            networkForm.getLocationCountry() +
+                            "&key=" + API_KEY,
+                    Geocode.class
+            );
+            Geometry geometry = geocode.getFirstResults().getGeometry();
+
+            return new Pair<>( geometry.getLocation().getLat(), geometry.getLocation().getLng() );
+        }catch( Exception e ){
+            return new Pair<>( 0.0, 0.0 );
+        }
+    }
+
+    public static Pair<Double, Double> getLatLonJson( String address ){
+        RestTemplate restTemplate = new RestTemplate();
+
+        try{
+            Geocode geocode = restTemplate.getForObject(
+                    "https://maps.googleapis.com/maps/api/geocode/json?address=" +
+                            address +
+                            "&key=" + API_KEY,
+                    Geocode.class
+            );
+            Geometry geometry = geocode.getFirstResults().getGeometry();
+
+            return new Pair<>( geometry.getLocation().getLat(), geometry.getLocation().getLng() );
+        }catch( Exception e ){
+            return new Pair<>( 0.0, 0.0 );
+        }
+    }
+
+    //****************************************************************
+    // endregion
+    //****************************************************************
+
+    public static String test( NetworkForm networkForm ){
+        RestTemplate restTemplate = new RestTemplate();
+
+        Geocode results = restTemplate.getForObject(
+                "https://maps.googleapis.com/maps/api/geocode/json?address=" +
+                        networkForm.getLocationAddress() + ", " +
+                        networkForm.getLocationZip() +
+                        networkForm.getLocationCity() + ", " +
+                        networkForm.getLocationCountry() +
+                        "&key=" + API_KEY,
+                Geocode.class
+        );
+
+        return "https://maps.googleapis.com/maps/api/geocode/json?address=" +
+                networkForm.getLocationAddress() + ", " +
+                networkForm.getLocationZip() +
+                networkForm.getLocationCity() + ", " +
+                networkForm.getLocationCountry() +
+                "&key=" + API_KEY;
+    }
+}
